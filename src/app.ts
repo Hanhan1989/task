7234997621fd38a366a36ac9c5d6a3012c2ca8fc
Hanhan Chen <chenhanhan1989@gmail.com>
2024-10-04 11:18:25 +0200

    Install bootstrap-icons

--- src/app.ts
+++ src/app.ts
@@ -33,6 +33,7 @@ app.use('/tasks', tasksRouter)
 
 // Configurar la carpeta 'node_modules' como estática para servir Bootstrap
 app.use('/bootstrap', express.static(path.join(__dirname, '../node_modules/bootstrap/dist')))
+app.use('/bootstrap-icons', express.static(path.join(__dirname, '../node_modules/bootstrap-icons')));
 
 // catch 404 and forward to error handler
 app.use((req: Request, res: Response, next: NextFunction) => {
