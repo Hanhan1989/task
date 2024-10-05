fe064cba4374e8876496331bcacdb9383d592433
Hanhan Chen <chenhanhan1989@gmail.com>
2024-10-05 13:04:04 +0200

    TinyEditor - Remove promotion button

--- public/javascripts/tarea/details/TinyMCEEditor.js
+++ public/javascripts/tarea/details/TinyMCEEditor.js
@@ -8,6 +8,7 @@ export class TinyMCEEditor {
       tinymce.init({
         selector: this.selector,  // Selecciona el campo de texto enriquecido
         license_key: 'gpl', // Añade esta línea para usar la licencia GPL
+        promotion: false, // Eliminar el botón de promoción
         plugins: 'link image code',
         toolbar: 'undo redo | styleselect | bold italic | link image | code',
       });
