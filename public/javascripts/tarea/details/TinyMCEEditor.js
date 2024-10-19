cc6dbe61da0ee4d3da299e4b5984f518626fba88
Hanhan Chen <chenhanhan1989@gmail.com>
2024-10-19 12:40:39 +0200

    Iinine editor mode for TINYMCE

--- public/javascripts/tarea/details/TinyMCEEditor.js
+++ public/javascripts/tarea/details/TinyMCEEditor.js
@@ -8,8 +8,9 @@ export class TinyMCEEditor {
         selector: this.selector,  // Selecciona el campo de texto enriquecido
         license_key: 'gpl', // Añade esta línea para usar la licencia GPL
         promotion: false, // Eliminar el botón de promoción
-        plugins: 'link image code',
+        plugins: ['link', 'image', 'code', 'quickbars'],
         menubar: false,
+        inline: true,
         toolbar: 'undo redo | styleselect | bold italic | link image | code',
       });
     }
