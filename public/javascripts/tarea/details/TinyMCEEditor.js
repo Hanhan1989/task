defc6d73181d9876ac745950202f2888e83fa130
Hanhan Chen <chenhanhan1989@gmail.com>
2024-10-19 11:02:33 +0200

    Style

--- public/javascripts/tarea/details/TinyMCEEditor.js
+++ public/javascripts/tarea/details/TinyMCEEditor.js
@@ -9,6 +9,7 @@ export class TinyMCEEditor {
         license_key: 'gpl', // Añade esta línea para usar la licencia GPL
         promotion: false, // Eliminar el botón de promoción
         plugins: 'link image code',
+        menubar: false,
         toolbar: 'undo redo | styleselect | bold italic | link image | code',
       });
     }
